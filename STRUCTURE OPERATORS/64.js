20b74cfe842b4a03c0aafa1994c67aaee5b6f82f
Ricardo <edmon.vasquez@uvltd.tech>
2023-06-19 14:41:37 -0600

    BEHING AND STRUCTURE OPERATORS FINISH

---
+++ STRUCTURE OPERATORS/64.js
@@ -0,0 +1,35 @@
+//OBJECTS AND MORE FUNCTIONS
+
+
+const arr = [1, 2, ...[3, 4]];
+
+
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+console.log(pizza, risotto, otherFood);
+
+
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrooms', 'onion', 'olives', 'spinach');
+restaurant.orderPizza('mushrooms');
