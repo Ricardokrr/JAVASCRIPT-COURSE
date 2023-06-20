5ba3db46ca9abc2cfd1ca17a10fbd95176f66f8c
Ricardo <edmon.vasquez@uvltd.tech>
2023-06-20 15:20:05 -0600

    FINALLY THIS PART

---
+++ OBJECT PROGRAMMING/122.js
@@ -0,0 +1,24 @@
+//PROTOTYPAL
+
+console.log(jonas.__proto__);
+
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+const arr = [3, 6, 6, 5, 6, 9, 9];
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+
+console.log(arr.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(x => x + 1);
+
